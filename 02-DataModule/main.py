ee46c384ae8f584d620f7d55a7d90ccecc70fb57
paleumm <zdotqdot@gmail.com>
2022-08-09 21:34:48 +0700

    Update main.py

--- 02-DataModule/main.py
+++ 02-DataModule/main.py
@@ -3,7 +3,7 @@ from Model import NeuralNetwork
 from pytorch_lightning import Trainer
 
 dataset = LitMNISTDataset("./data")
-model = NeuralNetwork()
+model = LitNeuralNetwork()
 trainer = Trainer(accelerator='gpu', devices=1, max_epochs=10)
 trainer.fit(model=model, datamodule=dataset)
 trainer.test(model=model, datamodule=dataset)
