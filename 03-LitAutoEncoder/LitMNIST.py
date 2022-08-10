76874b56dca83d0e12ccec497c67287974c3d637
paleumm <zdotqdot@gmail.com>
2022-08-11 00:23:48 +0700

    Update LitMNIST.py

--- 03-LitAutoEncoder/LitMNIST.py
+++ 03-LitAutoEncoder/LitMNIST.py
@@ -3,10 +3,11 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
 
 class LitMNISTDataset(LightningDataModule):
-    def __init__(self, root: str = "./data", batch_size: int = 32):
+    def __init__(self, root: str = "./data", batch_size: int = 32, num_workers=4):
         super().__init__()
         self.root = root
         self.batch_size = batch_size
+        self.num_workers = num_workers
 
         # transform PIL to Tensor, so it can be use with the model
         self.transform = transforms.Compose([
@@ -26,14 +27,14 @@ class LitMNISTDataset(LightningDataModule):
         self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
 
     def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=self.batch_size)
+        return DataLoader(self.mnist_train, batch_size=self.batch_size, num_workers=num_workers)
     
     def test_dataloader(self):
-        return DataLoader(self.mnist_test, batch_size=self.batch_size)
+        return DataLoader(self.mnist_test, batch_size=self.batch_size, num_workers=num_workers)
 
     def val_dataloader(self):
-        return DataLoader(self.mnist_val, batch_size=self.batch_size)
+        return DataLoader(self.mnist_val, batch_size=self.batch_size, num_workers=num_workers)
     
     def predict_dataloader(self):
-        return DataLoader(self.mnist_predict, batch_size=self.batch_size)
+        return DataLoader(self.mnist_predict, batch_size=self.batch_size, num_workers=num_workers)
     
