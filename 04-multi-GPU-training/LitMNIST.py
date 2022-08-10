e17ae482e5115d7a61b2cb4a6e5d6de4910bd660
paleumm <zdotqdot@gmail.com>
2022-08-11 00:46:34 +0700

    Update LitMNIST.py

--- 04-multi-GPU-training/LitMNIST.py
+++ 04-multi-GPU-training/LitMNIST.py
@@ -27,14 +27,14 @@ class LitMNISTDataset(LightningDataModule):
         self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
 
     def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=self.batch_size, num_workers=num_workers)
+        return DataLoader(self.mnist_train, batch_size=self.batch_size, num_workers=self.num_workers)
     
     def test_dataloader(self):
-        return DataLoader(self.mnist_test, batch_size=self.batch_size, num_workers=num_workers)
+        return DataLoader(self.mnist_test, batch_size=self.batch_size, num_workers=self.num_workers)
 
     def val_dataloader(self):
-        return DataLoader(self.mnist_val, batch_size=self.batch_size, num_workers=num_workers)
+        return DataLoader(self.mnist_val, batch_size=self.batch_size, num_workers=self.num_workers)
     
     def predict_dataloader(self):
-        return DataLoader(self.mnist_predict, batch_size=self.batch_size, num_workers=num_workers)
+        return DataLoader(self.mnist_predict, batch_size=self.batch_size, num_workers=self.num_workers)
     
