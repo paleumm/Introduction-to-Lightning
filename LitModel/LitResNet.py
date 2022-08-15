c99232d2bbf357c1d5f4ca9c9df456fdd271d06e
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-08-15 16:18:08 +0700

    Update LitResNet.py

--- LitModel/LitResNet.py
+++ LitModel/LitResNet.py
@@ -1,14 +1,7 @@
 from pytorch_lightning import LightningModule
-from torch import nn, cuda, randn
+from torch import nn
 from torch.optim import SGD
 from ResNet import ResNet, block
-
-'''
-PyTorch's Resnet implementation is reference from https://www.analyticsvidhya.com/blog/2021/06/build-resnet-from-scratch-with-python/
-'''
-
-# resnet = {'ResNet50':[3, 4, 6, 3], 'ResNet101':[3, 4, 23, 3], 'ResNet152':[3, 8, 36, 3]}
-# default_resnet = 'ResNet50'
 
 class LitResNet(LightningModule):
     def __init__(self, resnet_type:list = [3, 4, 6, 3], num_classes = 1000, image_channels = 3):
@@ -60,13 +53,3 @@ class LitResNet101(LitResNet):
 class LitResNet152(LitResNet):
     def __init__(self, num_classes = 1000, image_channels = 3):
         super(LitResNet152, self).__init__(num_classes=num_classes, image_channels=image_channels,resnet_type=[3, 8, 36, 3])
-
-
-
-# def test():
-#     net = ResNet101(img_channel=3, num_classes=1000)
-#     device = "cuda" if cuda.is_available() else "cpu"
-#     y = net(randn(4, 3, 224, 224)).to(device)
-#     print(y.size())
-
-# test()
